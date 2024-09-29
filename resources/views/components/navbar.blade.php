0f3b6f78678c3d37307dbdac0c16c39b87e8ca96
Cosmin Dolha <dolhacosmin207@gmail.com>
2024-09-29 19:04:59 +0200

    authentication

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,17 +12,34 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
+        @auth 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Ciao {{ Auth::user()->name }}
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+
+            <li>
+              <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Logout</a>
+            </li>
+            <form action="{{route('logout')}}" method="POST" id="form-logout" class="d-none">
+              @csrf 
+            </form>
           </ul>
         </li>
+        @endauth
+        @guest
+        <li class="nav-item dropdown">
+        
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Benvenuto Ospite
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
+            <li><a class="dropdown-item" href="{{route('login')}}">Accedi</a></li>
+          </ul>
+        </li>
+        @endguest 
         <li class="nav-item">
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
